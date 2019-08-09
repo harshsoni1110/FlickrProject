8c1e0f6f8a677750a5158465dd50c5f03ad25c84
Harsh Soni <harshsoni111093@gmail.com>
2019-08-10 01:36:11 +0530

    Fix: Minor fixes.

---
+++ app/src/main/java/com/example/projectflickr/constants/FlickrProjectConstants.kt
@@ -0,0 +1,10 @@
+package com.example.projectflickr.constants
+
+class FlickrProjectConstants {
+
+    companion object {
+        const val api_key_key = "api_key"
+        const val api_key_value = "SECRET_KEY"
+    }
+
+}
