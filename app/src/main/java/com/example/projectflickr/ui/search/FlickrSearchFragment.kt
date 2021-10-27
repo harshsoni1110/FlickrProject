588ffe4b8de7530556df1b9ff78ec0fdaf941e75
harsh <harshsoni111093@gmail.com>
2021-10-27 08:57:01 +0530

    Fix: Keyboard IME DONE action handled for search.

--- app/src/main/java/com/example/projectflickr/ui/search/FlickrSearchFragment.kt
+++ app/src/main/java/com/example/projectflickr/ui/search/FlickrSearchFragment.kt
@@ -1,14 +1,11 @@
 package com.example.projectflickr.ui.search
 
 import android.app.Activity
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
-import androidx.core.content.ContextCompat.getSystemService
+import android.view.inputmethod.EditorInfo
 import androidx.fragment.app.Fragment
 import com.example.projectflickr.R
 import com.example.projectflickr.utils.BasicUtil
@@ -42,34 +39,48 @@ class FlickrSearchFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
         rootView.btn_search.setOnClickListener {
-
-            val searchString = rootView.edt_search_tags.editableText.toString()
-            if (searchString == "") {
-                BasicUtil.hideSoftKeyboard(activity as Activity, rootView)
-                Snackbar.make(
-                    rootView,
-                    resources.getString(R.string.res_input_require),
-                    Snackbar.LENGTH_LONG
-                ).show()
-                return@setOnClickListener
-            }
-            if (BasicUtil.isNetworkAvailable(context!!)) {
-                activity?.supportFragmentManager?.beginTransaction()?.replace(
-                    R.id.lytContainer,
-                    FlickrSearchResultFragment.newInstance(searchString)
-                )?.addToBackStack(null)?.commit()
-            } else {
-                BasicUtil.hideSoftKeyboard(activity as Activity, rootView)
-                Snackbar.make(
-                    rootView,
-                    resources.getString(R.string.res_network_unavailable),
-                    Snackbar.LENGTH_LONG
-                ).show()
-            }
+            triggerSearch()
         }
 
+        rootView.edt_search_tags.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                triggerSearch()
+                return@setOnEditorActionListener true
+            }
+            false
+        }
         rootView.edt_search_tags.requestFocus()
         rootView.edt_search_tags.showKeyboard()
+    }
+
+    /**
+     * Search the entered keyword
+     */
+    private fun triggerSearch() {
+
+        val searchString = rootView.edt_search_tags.editableText.toString()
+        if (searchString == "") {
+            BasicUtil.hideSoftKeyboard(activity as Activity, rootView)
+            Snackbar.make(
+                rootView,
+                resources.getString(R.string.res_input_require),
+                Snackbar.LENGTH_LONG
+            ).show()
+            return
+        }
+        if (BasicUtil.isNetworkAvailable(context!!)) {
+            activity?.supportFragmentManager?.beginTransaction()?.replace(
+                R.id.lytContainer,
+                FlickrSearchResultFragment.newInstance(searchString)
+            )?.addToBackStack(null)?.commit()
+        } else {
+            BasicUtil.hideSoftKeyboard(activity as Activity, rootView)
+            Snackbar.make(
+                rootView,
+                resources.getString(R.string.res_network_unavailable),
+                Snackbar.LENGTH_LONG
+            ).show()
+        }
     }
 }
 
